13ee76ee75a754a98f83e923d101b5eb68ce216d
Затолокин Павел <ZatolokinPavel@yandex.ru>
2018-08-21 23:35:01 +0300

    add controller config to main.c

--- main.c
+++ main.c
@@ -10,8 +10,33 @@
  */
 
 
-#include <xc.h>
+// Конфигурация контроллера (регистры CONFIG1 и CONFIG2)
+#pragma config FCMEN    = ON    // Fail-Safe Clock Monitor
+#pragma config IESO     = ON    // Internal External Switchover
+#pragma config CLKOUTEN = OFF   // Clock Out to pin
+#pragma config BOREN    = ON    // Сброс по снижению напряжения питания (BOR)
+#pragma config CPD      = ON    // Защита памяти данных (EEPROM)
+#pragma config CP       = ON    // Защита памяти программ
+#pragma config MCLRE    = ON    // RA3/MCLR/VPP pin function is MCLR
+#pragma config PWRTE    = ON    // Power-up Timer
+#pragma config WDTE     = OFF   // Watchdog Timer
+#pragma config FOSC     = INTOSC// Тактовый генератор (INTOSC - внутренний 20 МГц)
+
+#pragma config LVP      = OFF   // Low-Voltage Programming
+#pragma config BORV     = LO    // Brown-out Reset Voltage Selection
+#pragma config STVREN   = ON    // Stack Overflow or Underflow will cause a Reset
+#pragma config PLLEN    = ON    // фазовая автоподстройка частоты (ФАПЧ) (Phased-Locked Loop)
+#pragma config WRT      = OFF   // Flash Memory Self-Write Protection
+
+// Подключаем заголовочные файлы компилятора
+#include <xc.h>             // стандартный для компилятора MPLAB XC8
+#include <stdio.h>
+#include <stdlib.h>
+#include <pic.h>            // общий для всех PIC
+#include <pic12f1822.h>
 
 void main(void) {
+    TRISA   = 0x00;         // порт A весь на выход
+    RA2     = 1;
     return;
 }
